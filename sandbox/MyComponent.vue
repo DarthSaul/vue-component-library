2ef752deadbd10e7560024ef30aa61d83bf1029e
Saul Graves <graves.saul@gmail.com>
2026-02-23 14:34:17 -0500

    my-component sandbox

---
+++ sandbox/MyComponent.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+	secure: {
+		type: Boolean,
+		default: false,
+	},
+	secureToggleBtn: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+const emit = defineEmits(['update:secure']);
+
+const inputType = computed(() => (props.secure ? 'password' : 'text'));
+</script>
+
+<template>
+	<div class="cnb-input-wrapper">
+		<input :type="inputType" v-bind="$attrs" />
+		<button
+			v-if="secureToggleBtn"
+			@click="emit('update:secure', !props.secure)"
+		>
+			<EyeOpenIcon v-if="props.secure" />
+			<EyeClosedIcon v-else />
+		</button>
+	</div>
+</template>
